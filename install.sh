5687249c4bcdd10f201277e4b0a9018cbafd0388
luohua <luo_1912@outlook.com>
2019-03-17 10:59:08 +0800

    Hello termux

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-cd $HOME
+termux-setup-storage
+#配置zsh
+[ $?=0 ] && (
 pkg update
 apt install vim zsh git curl wget python python-dev nmap openssh clang
+cp -af .oh-my-zsh ~/
+cp -af .zshrc ~/
+source ~/.zshrc
 
+#配置vim
+read -p "配置vim? [y/N]?" vi
+if [ $vi = y ]
+do
+cd $HOME
+[ $?=0 ] && git clone https://github.com/Guanvi/llvim
+[ $?=0 ] && cd llvim
+bash install.sh
+else
+	exit 0
+done
+)
 
-git clone https://github.com/Guanvi/llvim
-cd llvim&&bash install.sh
-
-git clone https://github.com/robbyrussell/oh-my-zsh.git
-
-
-
-
-
-
-echo ""
+[ $?=0 ] && echo "配置完成" || echo "配置失败"
